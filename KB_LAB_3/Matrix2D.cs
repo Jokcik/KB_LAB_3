4f9765a83eb0847fcf6d85861ec55130be456ff6
jokci <jokcik@yandex.ru>
2017-10-15 19:36:13 +0300

    add matrix2D

---
+++ KB_LAB_3/Matrix2D.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace KB_LAB_3
+{
+    public class Matrix2D
+    {
+        private float[][] _t;
+        private float[][] _r;
+        
+        public Matrix2D()
+        {
+            Reset();
+        }
+
+        public void Rotate(float angle, Point point)
+        {
+            Rotate(angle, new PointF(point.X, point.Y));
+        }
+        public void Rotate(float angle, PointF point)
+        {
+            angle = (float) Math.PI/ 180 * angle;
+            
+            _r[0][0] = (float)Math.Cos(angle);
+            _r[0][1] = (float)Math.Sin(angle);
+            _r[1][0] = -(float)Math.Sin(angle);
+            _r[1][1] = (float)Math.Cos(angle);
+
+            _t[2][0] = point.X;
+            _t[2][1] = point.Y;
+        }
+
+        public void Reset()
+        {
+            _t = initMatr();
+            _r = initMatr();
+        }
+
+        private float[][] initMatr()
+        {
+            return new []{
+                new[] {1f, 0f, 0f},
+                new[] {0f, 1f, 0f},
+                new[] {0f, 0f, 1f}
+            };
+        }
+
+        public void DrawPolygon(Graphics g, Point[] points)
+        {
+            DrawPolygon(g, points.Select(point => new PointF(point.X, point.Y)).ToArray());
+        }
+        
+        public void DrawPolygon(Graphics g, PointF[] points)
+        {
+            var vector = GetVector(points);
+            g.DrawPolygon(new Pen(Color.Black, 3), vector.Select(floats => new PointF(floats[0], floats[1])).ToArray());
+        }
+        
+        public void FillPolygon(Graphics g, PointF[] points)
+        {
+            var vector = GetVector(points);
+            g.FillPolygon(Brushes.White, vector.Select(floats => new PointF(floats[0], floats[1])).ToArray());
+        }
+
+        private float[][] GetVector(PointF[] points)
+        {
+            var vectors = points.Select(f => new[] {f.X, f.Y, 1}).ToArray();
+
+            _t[2][0] = -_t[2][0];
+            _t[2][1] = -_t[2][1];
+
+            var m = MultipleMatrix3x3(_t, _r);
+            _t[2][0] = -_t[2][0];
+            _t[2][1] = -_t[2][1];
+            m = MultipleMatrix3x3(m, _t);
+
+            var vector = vectors.Select(floats => MultipleMatrix1x3(new[] {floats}, m)[0]).ToArray();
+            return vector;
+        }
+
+        public float[][] MultipleMatrix1x3(float[][] matrL, float[][] matrR)
+        {
+            var a11 = matrL[0][0] * matrR[0][0] + matrL[0][1] * matrR[1][0] + matrL[0][2] * matrR[2][0];
+            var a12 = matrL[0][0] * matrR[0][1] + matrL[0][1] * matrR[1][1] + matrL[0][2] * matrR[2][1];
+            var a13 = matrL[0][0] * matrR[0][2] + matrL[0][1] * matrR[1][2] + matrL[0][2] * matrR[2][2];
+
+            return new[] {new[] {a11, a12, a13}};
+        }
+        
+        public float[][] MultipleMatrix3x3(float[][] matrL, float[][] matrR)
+        {
+            var a11 = matrL[0][0] * matrR[0][0] + matrL[0][1] * matrR[1][0] + matrL[0][2] * matrR[2][0];
+            var a12 = matrL[0][0] * matrR[0][1] + matrL[0][1] * matrR[1][1] + matrL[0][2] * matrR[2][1];
+            var a13 = matrL[0][0] * matrR[0][2] + matrL[0][1] * matrR[1][2] + matrL[0][2] * matrR[2][2];
+            
+            var a21 = matrL[1][0] * matrR[0][0] + matrL[1][1] * matrR[1][0] + matrL[1][2] * matrR[2][0];
+            var a22 = matrL[1][0] * matrR[0][1] + matrL[1][1] * matrR[1][1] + matrL[1][2] * matrR[2][1];
+            var a23 = matrL[1][0] * matrR[0][2] + matrL[1][1] * matrR[1][2] + matrL[1][2] * matrR[2][2];
+            
+            var a31 = matrL[2][0] * matrR[0][0] + matrL[2][1] * matrR[1][0] + matrL[2][2] * matrR[2][0];
+            var a32 = matrL[2][0] * matrR[0][1] + matrL[2][1] * matrR[1][1] + matrL[2][2] * matrR[2][1];
+            var a33 = matrL[2][0] * matrR[0][2] + matrL[2][1] * matrR[1][2] + matrL[2][2] * matrR[2][2];
+            
+            return new []
+            {
+                new [] {a11, a12, a13},
+                new [] {a21, a22, a23},
+                new [] {a31, a32, a33}
+            };
+        }
+    }
+}
---
+++ KB_LAB_3/StartForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KB_LAB_3
+{
+    public class StartForm : Form
+    {
+        public float angle = 0;
+        
+        private void InitializeComponent()
+        {
+            // Включаем двойную буферизацию
+            SetStyle(ControlStyles.DoubleBuffer |
+                     ControlStyles.UserPaint |
+                     ControlStyles.AllPaintingInWmPaint |
+                     ControlStyles.ResizeRedraw, // Перерисовывать при изменении размера окна
+                true);
+            UpdateStyles();
+
+//            Timer timer = new Timer();
+//            timer.Interval = 2;
+//            timer.Tick += (sender, args) =>
+//            {
+//                angle += 0.05f;
+//                Invalidate();       
+//            };
+//            timer.Start();
+        }
+
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            var bounds = g.VisibleClipBounds;
+            g.FillRectangle(Brushes.White, bounds);
+
+            // Радиус большей окружности
+            int radius;
+            if (bounds.Width > bounds.Height)
+                radius = (int) bounds.Height / 3; // 
+            else radius = (int) bounds.Width / 3;
+
+            // Если размеры окна маленькие, ничего не выводить
+            if (bounds.Width < 30 || bounds.Height < 30)
+                return;
+
+            // Координаты центра окружности
+            var center = new Point((int) bounds.Width / 2, (int) bounds.Height / 2);
+
+            // Рисуем круг
+            var rect = new Rectangle(center.X - radius, center.Y - radius, radius * 2, radius * 2);
+            g.DrawEllipse(Pens.Black, rect);
+            g.DrawPie(Pens.Black, rect, angle + -120, 60);
+            g.DrawPie(Pens.Black, rect, angle + 120, -60);
+
+            var smallRadius = radius / 3;
+            var rectSmall = new RectangleF(center.X - smallRadius, center.Y - smallRadius, smallRadius * 2,
+                smallRadius * 2);
+            g.FillEllipse(Brushes.White, rectSmall);
+            g.DrawEllipse(new Pen(Brushes.Black, 3), rectSmall);
+
+            createRect(g, center, radius);
+
+            base.OnPaint(e);
+        }
+
+        private void createRect(Graphics g, Point center, int radius)
+        {
+            var centerRect = new Point(center.X, center.Y - radius);
+            var wL = (float)Math.PI * radius / 180 * 120; 
+            var w = (2 * (float)Math.PI * radius - wL) / (2 * 50);
+
+            var p1 = new PointF(centerRect.X - w, centerRect.Y);
+            var p2 = new PointF(centerRect.X + w, centerRect.Y);
+            var p3 = new PointF(centerRect.X + w, centerRect.Y - 2 * w);
+            var p4 = new PointF(centerRect.X - w, centerRect.Y - 2 * w);
+            
+            var t11 = new PointF(p3.X + w / 1.5f, p3.Y);
+            var t12 = new PointF(p4.X - w / 1.5f, p4.Y);
+            var t13 = new PointF(centerRect.X, p4.Y - w * 3);
+            
+            var t21 = new PointF(t11.X, centerRect.Y - w * 4);
+            var t22 = new PointF(t12.X, centerRect.Y - w * 4);
+            var t23 = new PointF(t13.X, t13.Y - w * 2);
+
+            var d = new Matrix2D();
+            for (var i = 0; i < 5; ++i)
+            {
+                d.Rotate(angle + -45 - 22 * i, center);
+                d.DrawPolygon(g, new []{p1, p2, p3, p4});
+                if (i % 2 == 0)
+                {
+                    d.DrawPolygon(g, new[] {t21, t22, t23});
+                }
+                d.FillPolygon(g, new []{t11, t12, t13});
+                d.DrawPolygon(g, new []{t11, t12, t13});
+            }
+            
+            for (var i = 0; i < 5; ++i)
+            {
+                d.Rotate(angle + 45 + 22 * i, center);
+                d.DrawPolygon(g, new []{p1, p2, p3, p4});
+                if (i % 2 == 0)
+                {
+                    d.DrawPolygon(g, new[] {t21, t22, t23});
+                }
+                d.FillPolygon(g, new []{t11, t12, t13});
+                d.DrawPolygon(g, new []{t11, t12, t13});
+            }
+
+            DrawFox(g, d, center, radius);
+        }
+
+        private void DrawFox(Graphics graphics, Matrix2D matrix2D, Point center, int radius)
+        {
+            
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            angle += e.Delta > 0 ? 1 : -1;
+            Invalidate();
+            base.OnMouseWheel(e);
+        }
+    }
+}
